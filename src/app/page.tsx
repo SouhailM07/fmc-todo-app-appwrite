b4b0c7edfbaeb51731a5466160ece75e867f0340
souhail <lightshadow416@gmail.com>
2024-03-03 17:05:31 +0100

    updating auth links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -94,10 +94,8 @@ const Login = () => {
       // Replace the URL hosts with the provided hosts
       account.createOAuth2Session(
         "google",
-        // @ts-ignore
-        ["fmc-todo-app-appwrite.vercel.app/home"],
-        // @ts-ignore
-        ["fmc-todo-app-appwrite.vercel.app"]
+        "https://fmc-todo-app-appwrite.vercel.app/home",
+        "https://fmc-todo-app-appwrite.vercel.app/"
       );
     } catch (error) {
       console.error("Error creating OAuth2 session:", error);
